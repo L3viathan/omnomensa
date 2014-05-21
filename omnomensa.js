e8aab6ae36d7a5332df79e73784a1b693a70c8b8
L3viathan <git@l3vi.de>
2014-05-21 03:40:32 +0200

    Version 1.0

--- omnomensa.js
+++ omnomensa.js
@@ -23,21 +23,21 @@ $.ajax({
         for (var i=elements.length-1;i>=0;i--) {
             elem = elements[i].innerText.replace(/\s+\d+(,\d+)*\s*$/g, '').replace(/,.+/g, '').replace("mensaVital: ","").trim();
             if (elem == "täglich wechselnd") continue;
-            console.log(elem);
             $(".meals").append('<li class="fancybox"><span class="name">'+elem+'</span></li>');
 		}
 		$(".fancybox").click(function(event) {
 			$(this).addClass('selected');
 			$("body").addClass('meal-selected');
 			$(this).off("click");
-			selectedMeal = $(this).innerText;
+			$(".rating").attr('data-meal',$(this).innerText);
 		});
 		$(".rating li").click(function(event) {
 			$(".rating li").off("click");
-			$.post('http://api.l3vi.de/mensa', {rating: $(this).attr(id), meal: selectedMeal}).done(function(data){
-                $("#good").innerText(data['good'] + "Votes");
-                $("#okay").innerText(data['okay'] + "Votes");
-                $("#bad").innerText(data['bad'] + "Votes");
+			console.log({rating: $(this).attr('id'), meal: $(".rating").attr('data-meal')});
+			$.post('http://api.l3vi.de/mensa.json', {rating: $(this).attr('id'), meal: selectedMeal}).done(function(data){
+                $("#good").text(data['good'] + " Votes");
+                $("#okay").text(data['okay'] + " Votes");
+                $("#bad").text(data['bad'] + " Votes");
 			});
 		});
 	}
