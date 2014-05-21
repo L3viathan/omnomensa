be0cf1a68ea96fdbeb854ce230cafa32081efd24
L3viathan <git@l3vi.de>
2014-05-21 13:02:06 +0200

    nasty bug fixed

--- omnomensa.js
+++ omnomensa.js
@@ -1,5 +1,3 @@
-var selectedMeal = '';
-
 $(function(){
     
 $('.meals li').click(function(event) {
@@ -16,26 +14,27 @@ $.ajax({
 		var dd = today.getDate();
 		var mm = today.getMonth()+1;//January is 0!`
 		var yyyy = today.getFullYear();
-		if(dd<10){dd='0'+dd}
-		if(mm<10){mm='0'+mm}
+		if(dd<10){dd='0'+dd;}
+		if(mm<10){mm='0'+mm;}
 
 		//I'm so fucking sorry:
-		var elements = $("<div>").html(data).find(".tab-" + Math.round(new Date(yyyy + "-" + mm + "-" + dd + " 00:00:00").getTime()/1000) + " .desc");
-        for (var i=elements.length-1;i>=0;i--) {
-            elem = elements[i].innerText.replace(/\s+\d+(,\d+)*\s*$/g, '').replace(/,.+/g, '').replace("mensaVital: ","").trim();
-            if (elem == "täglich wechselnd") continue;
+		var elements = $("<div>").html(data).find(".tab-" + Math.round(new Date(yyyy, mm-1, dd).getTime()/1000) + " .desc");
+        elements.each(function(){
+            elem = $(this).text().replace(/\s+\d+(,\d+)*\s*$/g, '').replace(/,.+/g, '').replace("mensaVital: ","").trim();
+            if (elem == "täglich wechselnd") return;
             $(".meals").append('<li class="fancybox"><span class="name">'+elem+'</span></li>');
-		}
+		});
 		$(".fancybox").click(function(event) {
 			$(this).addClass('selected');
 			$("body").addClass('meal-selected');
 			$(this).off("click");
-			$(".rating").attr('data-meal',$(this).innerText);
+			$(".rating").data('meal',$(this).text());
 		});
 		$(".rating li").click(function(event) {
 			$(".rating li").off("click");
-			console.log({rating: $(this).attr('id'), meal: $(".rating").attr('data-meal')});
-			$.post('http://api.l3vi.de/mensa.json', {rating: $(this).attr('id'), meal: selectedMeal}).done(function(data){
+			console.log({rating: $(this).attr('id'), meal: $(".rating").data('meal')});
+			$.post('http://api.l3vi.de/mensa.json', 'rating=' + $(this).attr('id') + '&meal=' + $(".rating").data('meal')).done(function(data){
+				console.log(data);
                 $("#good").text(data['good'] + " Votes");
                 $("#okay").text(data['okay'] + " Votes");
                 $("#bad").text(data['bad'] + " Votes");
