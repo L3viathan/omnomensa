9b30d97debe33c8250081b4dca86a556172acf9d
L3viathan <git@l3vi.de>
2014-05-27 19:36:35 +0200

    day selection support

--- omnomensa.js
+++ omnomensa.js
@@ -3,6 +3,52 @@ String.prototype.repeat = function( num )
     return new Array( num + 1 ).join( this );
 }
 
+function getMeals() {
+    $(".meals").empty();
+    $(".day").removeClass('day-selected');
+    $.ajax({
+        url: "http://mensaproxy.l3vi.de/de/Essen/Essen-in-Saarbrucken/Speiseplan-aktuell",
+        dataType: "text",
+        success: function(data) {
+            var day = $('.days').data("day") ? $('.days').data("day") : parseInt(/today = '(\d)'/.exec(data)[1]) + 1;
+            var elements = $("<div>").html(data).find(".tab:nth-child(" + day + ") .desc");
+            var anything = false;
+            elements.each(function(){
+                elem = $(this).text().replace(/\s+\d+(,\d+)*\s*$/g, '').replace(/,.+/g, '').replace("mensaVital: ","").replace("Tagesessen: ","").replace("ZIS Spezialitätentag ","").replace("Heute für Sie: ","").replace("Heute für Sie ","").replace("Vegan: ","").replace('( ','(').replace(' )',')').replace(/Büffet.+$/g,'Büffet').trim();
+                var price = $(this).prev().children('.price:first-child').text();
+                if (elem == "täglich wechselnd") return;
+                anything = true;
+                $.post('http://api.l3vi.de/mensa.json', 'rating=0&meal='+elem).done(function(rating_data){
+                    if (rating_data['number'] == 0) {
+                        rating_data['number'] = 1;
+                    }
+                    if (rating_data['meta']) {
+                        return;
+                    }
+                    $(".meals").append('<li class="fancybox"><div class="item"><span class="name">'+rating_data['name']+' </span> <div class="meal-all-stars"><span class="meal-stars"> ' + "★".repeat(Math.round(rating_data['stars']/rating_data['number'])) + '</span><span class="meal-stars-black">' + (rating_data['stars'] != 0 ? "★".repeat(5-Math.round(rating_data['stars']/rating_data['number'])) : '') + '</span><span class="price">' + price + '</span></div></div></li>');
+                    $(".fancybox").click(function(event) {
+                        History.pushState({state:1}, "Mensadingsi: Rate meal", "?rate");
+                        $(this).addClass('selected');
+                        $("body").addClass('meal-selected');
+                        $(this).off("click");
+                        $("#rating").data('meal',$(this).find(".name").text());
+                    });
+                });
+            });
+            if(!anything) {
+                $(".meals").append('<span class="message">Go eat somewhere else today</span>');
+            }
+            $("#rating span").click(function(event) {
+                $("#rating span").off("click");
+                $.post('http://api.l3vi.de/mensa.json', 'rating=' + $(this).attr('id') + '&meal=' + $("#rating").data('meal')).done(function(data){
+                    $("#rating").html("★".repeat(Math.round(data['stars']/data['number'])) + "<em>(" + data['number'] + " votes)</em>")
+                });
+            });
+            $("#day-"+day).addClass('day-selected');
+        }
+    });
+}
+
 $(function(){
     
 $('.meals li').click(function(event) {
@@ -11,46 +57,7 @@ $('.meals li').click(function(event) {
     $("body").addClass('meal-selected');
 });
 
-$.ajax({
-    url: "http://mensaproxy.l3vi.de/de/Essen/Essen-in-Saarbrucken/Speiseplan-aktuell",
-    dataType: "text",
-    success: function(data) {
-        var day = parseInt(/today = '(\d)'/.exec(data)[1]) + 1;
-        var elements = $("<div>").html(data).find(".tab:nth-child(" + day + ") .desc");
-        var anything = false;
-        elements.each(function(){
-            elem = $(this).text().replace(/\s+\d+(,\d+)*\s*$/g, '').replace(/,.+/g, '').replace("mensaVital: ","").replace("Tagesessen: ","").replace("ZIS Spezialitätentag ","").replace("Heute für Sie: ","").replace("Heute für Sie ","").replace("Vegan: ","").replace('( ','(').replace(' )',')').replace(/Büffet.+$/g,'Büffet').trim();
-            var price = $(this).prev().children('.price:first-child').text();
-            if (elem == "täglich wechselnd") return;
-            anything = true;
-            $.post('http://api.l3vi.de/mensa.json', 'rating=0&meal='+elem).done(function(rating_data){
-                if (rating_data['number'] == 0) {
-                    rating_data['number'] = 1;
-                }
-                if (rating_data['meta']) {
-                	return;
-                }
-                $(".meals").append('<li class="fancybox"><div class="item"><span class="name">'+rating_data['name']+' </span> <div class="meal-all-stars"><span class="meal-stars"> ' + "★".repeat(Math.round(rating_data['stars']/rating_data['number'])) + '</span><span class="meal-stars-black">' + (rating_data['stars'] != 0 ? "★".repeat(5-Math.round(rating_data['stars']/rating_data['number'])) : '') + '</span><span class="price">' + price + '</span></div></div></li>');
-                $(".fancybox").click(function(event) {
-                    History.pushState({state:1}, "Mensadingsi: Rate meal", "?rate");
-                    $(this).addClass('selected');
-                    $("body").addClass('meal-selected');
-                    $(this).off("click");
-                    $("#rating").data('meal',$(this).find(".name").text());
-                });
-            });
-        });
-		if(!anything) {
-			$(".meals").append('<span class="message">Go eat somewhere else today</span>');
-		}
-        $("#rating span").click(function(event) {
-            $("#rating span").off("click");
-            $.post('http://api.l3vi.de/mensa.json', 'rating=' + $(this).attr('id') + '&meal=' + $("#rating").data('meal')).done(function(data){
-                $("#rating").html("★".repeat(Math.round(data['stars']/data['number'])) + "<em>(" + data['number'] + " votes)</em>")
-            });
-        });
-    }
-});
+getMeals();
 
 (function(window,undefined){
     History.Adapter.bind(window,'statechange',function(){
@@ -70,5 +77,10 @@ $.ajax({
     History.replaceState({state:0}, "Mensadingsi", "?home");
 
 })(window);
+
+$(".day").click(function(event) {
+    $(".days").data("day",parseInt($(this).attr('id').slice(-1)));
+    getMeals();
+});
 
 });
